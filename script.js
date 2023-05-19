bab59e70f713891613a14b136c6e10ce23a996a7
CarlosEduardoSilverio <cadusilverio444@gmail.com>
2023-05-19 06:10:34 -0300

    SegundaVersaoSmilingFriends

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ $('.carousel234').slick({
 $('.Charlie-carousel').slick({
   dots:true
 });
+
+$('.carousel').slick({
+  dots:true
+});
+
+var buttonB = document.getElementById("botao");
+var buttonA = document.getElementById("botao");
+
+buttonB.addEventListener("click", function(){
+  
+  var containerB = document.getElementById("mostrar");
+
+  containerB.classList.toggle("show");
+})
+
+buttonA.addEventListener("click", function(){
+  
+  var containerA = document.getElementById("esconder");
+
+  containerA.classList.toggle("hide");
+})
+
+function toggleDropdown() {
+  document.getElementById("Inicial").classList.toggle("hide");
+}
+
+// window.onclick = function (event) {
+//   if (!event.target.matches(".dropbtn")) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     for (var i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show2")) {
+//         openDropdown.classList.remove("show2");
+//       }
+//     }
+//   }
+// };
